891c96e62edbe835d2befe2695fc713924c6853b
Sumant <DoNotExist>
2015-08-09 10:59:57 +0530

    File addes

---
+++ TypeScriptLinq/System/Collections/IEqualityComparer.ts
@@ -0,0 +1,11 @@
+﻿module System {
+    export module Collections {
+        export interface IEqualityComparer<T>
+        {
+            Equals: (x: T, y: T) => boolean;
+            GetHashCode: (obj: T) => number;
+        }
+    }
+}
+
+import IEqualityComparer = System.Collections.IEqualityComparer;
